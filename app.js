0a820e11865d581d27f44d5e888610a550e19afa
arechiga2 <arechiga21jj@gmail.com>
2021-03-18 18:51:39 -0600

    generated the HTML file

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);   
-    }
+const fs = require('fs');
 
-    console.log('==============');
+const profileDataArgs = process.argv.slice(2);
 
-    profileDataArr.forEach((profileItem => console.log(profileItem));
-    );
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs)
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
